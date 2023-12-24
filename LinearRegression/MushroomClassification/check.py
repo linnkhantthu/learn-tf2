a34f2e1a6fdf0a4ebd829bbd635abc559e72484b
linnkhantthu <linnkhantthu1999mdy@gmail.com>
2023-12-24 20:33:49 +0630

    added model class

--- LinearRegression/MushroomClassification/check.py
+++ LinearRegression/MushroomClassification/check.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from sys import argv
 import os
+from model import Mushroom
 
 # Load model path
 path = os.path.dirname(os.path.abspath(__file__))
@@ -42,34 +43,38 @@ def predict(cap_shape, cap_surface, cap_color, bruises,
     Returns:
         _type_: _description_
     """
-
+    mushroom = Mushroom(cap_shape, cap_surface, cap_color, bruises,
+                        odor, gill_attachment, gill_spacing, gill_size, gill_color,
+                        stalk_shape, stalk_root, stalk_surface_above_ring,
+                        stalk_surface_below_ring, stalk_color_above_ring, stalk_color_below_ring,
+                        veil_type, veil_color, ring_number, ring_type, spore_print_color, population, habitat)
     # Column Names
-    COLUMN_NAMES = ['cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor',
-                    'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color',
-                    'stalk-shape', 'stalk-root', 'stalk-surface-above-ring',
-                    'stalk-surface-below-ring', 'stalk-color-above-ring',
-                    'stalk-color-below-ring', 'veil-type', 'veil-color', 'ring-number',
-                    'ring-type', 'spore-print-color', 'population', 'habitat']
+    # COLUMN_NAMES = ['cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor',
+    #                 'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color',
+    #                 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring',
+    #                 'stalk-surface-below-ring', 'stalk-color-above-ring',
+    #                 'stalk-color-below-ring', 'veil-type', 'veil-color', 'ring-number',
+    #                 'ring-type', 'spore-print-color', 'population', 'habitat']
 
     # Create an example of a standard proto storing data for training and inference.
     example = tf.train.Example()
 
     # Creating a dataframe dict
-    dfpredict = {
-        'cap-shape': cap_shape, 'cap-surface': cap_surface, 'cap-color': cap_color, 'bruises': bruises, 'odor': odor,
-        'gill-attachment': gill_attachment, 'gill-spacing': gill_spacing, 'gill-size': gill_size, 'gill-color': gill_color,
-        'stalk-shape': stalk_shape, 'stalk-root': stalk_root, 'stalk-surface-above-ring': stalk_surface_above_ring,
-        'stalk-surface-below-ring': stalk_surface_below_ring, 'stalk-color-above-ring': stalk_color_above_ring,
-        'stalk-color-below-ring': stalk_color_below_ring, 'veil-type': veil_type, 'veil-color': veil_color, 'ring-number': ring_number,
-        'ring-type': ring_type, 'spore-print-color': spore_print_color, 'population': population, 'habitat': habitat
-    }
+    # dfpredict = {
+    #     'cap-shape': cap_shape, 'cap-surface': cap_surface, 'cap-color': cap_color, 'bruises': bruises, 'odor': odor,
+    #     'gill-attachment': gill_attachment, 'gill-spacing': gill_spacing, 'gill-size': gill_size, 'gill-color': gill_color,
+    #     'stalk-shape': stalk_shape, 'stalk-root': stalk_root, 'stalk-surface-above-ring': stalk_surface_above_ring,
+    #     'stalk-surface-below-ring': stalk_surface_below_ring, 'stalk-color-above-ring': stalk_color_above_ring,
+    #     'stalk-color-below-ring': stalk_color_below_ring, 'veil-type': veil_type, 'veil-color': veil_color, 'ring-number': ring_number,
+    #     'ring-type': ring_type, 'spore-print-color': spore_print_color, 'population': population, 'habitat': habitat
+    # }
 
     print(example.features.feature.keys())
 
     # Storing features into example proto
-    for feature_name in COLUMN_NAMES:
+    for feature_name in mushroom.__dict__:
         example.features.feature[feature_name].bytes_list.value.extend(
-            [dfpredict[feature_name].encode()])
+            [mushroom.__dict__[feature_name].encode()])
 
     # Predicting
     result = imported.signatures["predict"](
